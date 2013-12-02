4ed6e8449e2825f90d98c38fb387f7616922f406
Martin <mac.cat@freenet.de>
2013-12-02 12:33:57 +0100

    added files for encoder

---
+++ whisper-library/include/whisper-library/encoder.hpp
@@ -0,0 +1,25 @@
+// author: Martin Oehler
+
+#ifndef ENCODER
+#define ENCODER
+
+
+#include "common.hpp"
+
+namespace whisperLibrary {
+
+class WHISPERAPI Encoder {
+
+public:
+	Encoder();
+	int encodePackage();
+
+private:
+
+};
+
+}
+
+
+
+#endif
---
+++ whisper-library/src/encoder.cpp
@@ -0,0 +1,15 @@
+// author: Martin Oehler
+
+#include <encoder.hpp>
+
+namespace whisperLibrary {
+
+	Encoder::Encoder() {
+	
+	}
+	
+	int Encoder::encodePackage() {
+		return 0;
+	}
+
+}
