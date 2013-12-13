2286a46bf12a065b611433fcd8da2d2bf1eafff9
Jan Simon Bunten <jsbunten@gmail.com>
2013-12-13 08:00:40 +0100

    changed implementation from bitset to vector<bool>

--- whisper-library/include/whisper-library/tcppackage.hpp
+++ whisper-library/include/whisper-library/tcppackage.hpp
@@ -2,88 +2,94 @@
 
 #ifndef TCP_PACKAGE
 #define TCP_PACKAGE
+#define maxUInt = 65000;
 #endif
 
+#define uint unsigned int
+
 #include "common.hpp"
+#include <vector>
 
 namespace whisperLibrary {
 
-  class WHISPERAPI TcpPackage { 
-  
-  public:
-	  TcpPackage();
-	 
-	  int get_sourcePort(); // 16bit
-	  int get_destPort(); // 16 bit
-	  
-	  int get_sequenceNumber(); // 32 bit
-	  int get_acknowlageNumber(); // 32 bit
-	  
-	  int get_dataOffset(); // 4 bit
-	  int get_reserved(); // 3 bit
-	  
-	  int get_flags(); // 9 bit
-	  bool get_ns();
-	  bool get_cwr();
-	  bool get_ece();
-	  bool get_urg();
-	  bool get_ack();
-	  bool get_psh();
-	  bool get_rst();
-	  bool get_syn();
-	  bool get_fin();
+	class /*WHISPERAPI*/ TcpPackage { 
 
-	  int get_windowSize();
-	  int get_checksum();
-	  int* get_options();
-	  
-	  void set_sourcePort(int val); // 16bit
-	  void set_destPort(int val); // 16 bit
-	  
-	  void set_sequenceNumber(int val); // 32 bit
-	  void set_acknowlageNumber(int val); // 32 bit
-	  
-	  void set_dataOffset(int val); // 4 bit
-	  void set_reserved(int val); // 3 bit
-	  
-	  void set_flags(int val); // 9 bit
-	  void set_ns(bool val);
-	  void set_cwr(bool val);
-	  void set_ece(bool val);
-	  void set_urg(bool val);
-	  void set_ack(bool val);
-	  void set_psh(bool val);
-	  void set_rst(bool val);
-	  void set_syn(bool val);
-	  void set_fin(bool val);
+	public:
+		TcpPackage();
+		/* Locations of the header informations:
+		 * 0-15 Source port, 16-31 Destination port
+		 * 32-63 Sequence number
+		 * 63-95 Acknowledgement number
+		 * 96-99 Data offset
+		 * 100-102 Reserved
+		 * 103 NS, 104 CWR, 105 ECE, 106 URG, 107 ACK
+		 * 108 PSH, 109 RST, 110 SYN, 111 FIN, 112-127 Window Size
+		 * 128-143 checksum, 144-159 Urgent Pointer
+		 * Options field:
+		 * 0-319 depending on data-offset
+		 * 0-7 option kind, 8-15 option length
+		 * (option-length) option data
+		 */
 
-	  void set_windowSize(int val);
-	  void set_checksum(int val);
-	  void set_options(int* val);
-	  
-  
-  private:
-	  int sourcePort; // 16bit
-	  int destPort; // 16 bit
-	  
-	  int sequenceNumber; // 32 bit
-	  int acknowlageNumber; // 32 bit
-	  
-	  int dataOffset; // 4 bit
-	  int reserved; // 3 bit
-	  
-	  bool ns;
-	  bool cwr;
-	  bool ece;
-	  bool urg;
-	  bool ack;
-	  bool psh;
-	  bool rst;
-	  bool syn;
-	  bool fin;
+		uint get_sourcePort(); // 16bit
+		uint get_destPort(); // 16 bit
 
-	  int windowSize;
-	  int checksum;
-	  int* options;
-  };
+		uint get_sequenceNumber(); // 32 bit
+		uint get_acknowlageNumber(); // 32 bit
+
+		uint get_dataOffset(); // 4 bit
+		uint get_reserved(); // 3 bit
+
+		uint get_flags(); // 9 bit
+		bool get_ns();
+		bool get_cwr();
+		bool get_ece();
+		bool get_urg();
+		bool get_ack();
+		bool get_psh();
+		bool get_rst();
+		bool get_syn();
+		bool get_fin();
+
+		uint get_windowSize();
+		uint get_checksum();
+		std::vector<uint> get_options();
+		std::vector<bool>* get_package();
+		std::vector<uint> get_data();
+
+		void set_sourcePort(uint val); // 16bit
+		void set_destPort(uint val); // 16 bit
+
+		void set_sequenceNumber(uint val); // 32 bit
+		void set_acknowlageNumber(uint val); // 32 bit
+
+		void set_dataOffset(uint val); // 4 bit
+		void set_reserved(uint val); // 3 bit
+
+		void set_flags(uint val); // 9 bit
+		void set_ns(bool val);
+		void set_cwr(bool val);
+		void set_ece(bool val);
+		void set_urg(bool val);
+		void set_ack(bool val);
+		void set_psh(bool val);
+		void set_rst(bool val);
+		void set_syn(bool val);
+		void set_fin(bool val);
+
+		void set_windowSize(uint val);
+		void set_checksum(uint val);
+		void set_options(uint* val, uint size);
+		
+		void set_data(uint* val, uint size);
+
+
+	private:
+		std::vector<bool> header;
+		std::vector<bool> options;
+		std::vector<bool> data;
+		bool* intToBoolArray(uint val);
+		uint vectorToUInt(int start, int end, std::vector<bool> &set);
+		void uIntToVector(int start, int end, std::vector<bool> &set, uint val);
+	};
 }
--- whisper-library/src/tcppackage.cpp
+++ whisper-library/src/tcppackage.cpp
@@ -2,130 +2,205 @@
 
 namespace whisperLibrary {
 	TcpPackage::TcpPackage(){
-		
+		header.resize(159);
+		options.resize(319);
 	}
+	
+	/* Locations of the header informations:
+		 * 0-15 Source port, 16-31 Destination port
+		 * 32-63 Sequence number
+		 * 64-95 Acknowledgement number
+		 * 96-99 Data offset
+		 * 100-102 Reserved
+		 * 103 NS, 104 CWR, 105 ECE, 106 URG, 107 ACK
+		 * 108 PSH, 109 RST, 110 SYN, 111 FIN, 112-127 Window Size
+		 * 128-143 checksum, 144-159 Urgent Pointer
+		 * Options field:
+		 * 0-319 depending on data-offset
+		 * 0-7 option kind, 8-15 option length
+		 * (option-length) option data
+		 */
 	 
-	int TcpPackage::get_sourcePort(){
-		return sourcePort;
-	} // 16bit
-	int TcpPackage::get_destPort(){
-		return destPort;
-	} // 16 bit
-	  
-	int TcpPackage::get_sequenceNumber(){
-		return sequenceNumber;
-	}; // 32 bit
-	int TcpPackage::get_acknowlageNumber(){
-		return acknowlageNumber;
-	}; // 32 bit
-	  
-	int TcpPackage::get_dataOffset(){
-		return dataOffset;
-	}; // 4 bit
-	int TcpPackage::get_reserved(){
-		return reserved;
-	}; // 3 bit
-	  
-	int TcpPackage::get_flags(){
-		return 0;
-	}; // 9 bit
+	// header: bits 0-15
+	uint TcpPackage::get_sourcePort(){
+		return vectorToUInt(0, 15, header);
+	} 
+	// header: bits 16-31
+	uint TcpPackage::get_destPort(){
+		return vectorToUInt(16, 31, header);
+	} 
+	// header: bits 32-63  
+	uint TcpPackage::get_sequenceNumber(){
+		return vectorToUInt(32, 63, header);
+	};
+	// header: bits 64-95 
+	uint TcpPackage::get_acknowlageNumber(){
+		return vectorToUInt(64, 95, header);
+	};
+	// header: bits 96-99  
+	uint TcpPackage::get_dataOffset(){
+		return vectorToUInt(96, 99, header);
+	};
+	// header: bits 100-102
+	uint TcpPackage::get_reserved(){
+		return vectorToUInt(100, 102, header);
+	}; 
+	// header: bits 103-111  
+	uint TcpPackage::get_flags(){
+		return vectorToUInt(103, 111, header);
+	};
+	// header: bit 103
 	bool TcpPackage::get_ns(){
-		return ns;
+		return header.at(103);
 	};
+	// header: bit 104
 	bool TcpPackage::get_cwr(){
-		return cwr;
+		return header.at(104);
 	};
+	// header: bit 105
 	bool TcpPackage::get_ece(){
-		return ece;
+		return header.at(105);
 	};
+	// header: bit 106
 	bool TcpPackage::get_urg(){
-		return urg;
+		return header.at(106);
 	};
+	// header: bit 107
 	bool TcpPackage::get_ack(){
-		return ack;
+		return header.at(107);
 	};
+	// header: bit 108
 	bool TcpPackage::get_psh(){
-		return psh;
+		return header.at(108);
 	};
+	// header: bit 109
 	bool TcpPackage::get_rst(){
-		return rst;
+		return header.at(109);
 	};
+	// header: bit 110
 	bool TcpPackage::get_syn(){
-		return syn;
+		return header.at(110);
 	};
+	// header: bit 111
 	bool TcpPackage::get_fin(){
-		return fin;
+		return header.at(111);
 	};
-
-	int TcpPackage::get_windowSize(){
-		return windowSize;
+	// header: bits 112-127
+	uint TcpPackage::get_windowSize(){
+		return vectorToUInt(112, 127, header);
 	};
-	int TcpPackage::get_checksum(){
-		return checksum;
+	// header: bits 128-143
+	uint TcpPackage::get_checksum(){
+		return vectorToUInt(128, 143, header);
 	};
-	int* TcpPackage::get_options(){
-		return options;
+	// options: bits 0-320
+	std::vector<uint> TcpPackage::get_options(){
+		std::vector<uint> ret;
+		for (int i = 0; i < (options.size() / 32); i++){
+			ret.push_back(vectorToUInt(i, (i + 31), options));
+		}
+		return ret;
 	};
-	  
-	void TcpPackage::set_sourcePort(int val){
-		sourcePort = val;
-	}; // 16bit
-	void TcpPackage::set_destPort(int val){
-		destPort = val;
-	}; // 16 bit
-	  
-	void TcpPackage::set_sequenceNumber(int val){
-		sequenceNumber = val;
-	}; // 32 bit
-	void TcpPackage::set_acknowlageNumber(int val){
-		acknowlageNumber = val;
-	}; // 32 bit
-	  
-	void TcpPackage::set_dataOffset(int val){
-		dataOffset = val;
-	}; // 4 bit
-	void TcpPackage::set_reserved(int val){
-		reserved = val;
-	}; // 3 bit
-	  
-	void TcpPackage::set_flags(int val){
-		
-	}; // 9 bit
+	// header: bits 0-15  
+	void TcpPackage::set_sourcePort(uint val){
+		uIntToVector(0,15,header,val);
+	};
+	// header: bits 16-31
+	void TcpPackage::set_destPort(uint val){
+		uIntToVector(16,31,header,val);
+	};
+	// header: bits 32-63  
+	void TcpPackage::set_sequenceNumber(uint val){
+		uIntToVector(32,63,header,val);
+	};
+	// header: bits 64-95
+	void TcpPackage::set_acknowlageNumber(uint val){
+		uIntToVector(64,95,header,val);
+	};
+	// header: bits 96-99  
+	void TcpPackage::set_dataOffset(uint val){
+		uIntToVector(96,99,header,val);
+	};
+	// header: bits 100-102
+	void TcpPackage::set_reserved(uint val){
+		uIntToVector(100,102,header,val);
+	};
+	// header: bits 103-111  
+	void TcpPackage::set_flags(uint val){
+		uIntToVector(103,111,header,val);
+	};
+	// header: bit 103
 	void TcpPackage::set_ns(bool val){
-		ns = val;
+		header[103] = val;
 	};
+	// header: bit 104
 	void TcpPackage::set_cwr(bool val){
-		cwr = val;
+		header[104] = val;
 	};
+	// header: bit 105
 	void TcpPackage::set_ece(bool val){
-		ece = val;
+		header[105] = val;
 	};
+	// header: bit 106
 	void TcpPackage::set_urg(bool val){
-		urg = val;
+		header[106] = val;
 	};
+	// header: bit 107
 	void TcpPackage::set_ack(bool val){
-		ack = val;
+		header[107] = val;
 	};
+	// header: bit 108
 	void TcpPackage::set_psh(bool val){
-		psh = val;
+		header[108] = val;
 	};
+	// header: bit 109
 	void TcpPackage::set_rst(bool val){
-		rst = val;
+		header[109] = val;
 	};
+	// header: bit 110
 	void TcpPackage::set_syn(bool val){
-		syn = val;
+		header[110] = val;
 	};
+	// header: bit 111
 	void TcpPackage::set_fin(bool val){
-		fin = val;
+		header[111] = val;
 	};
-
-	void TcpPackage::set_windowSize(int val){
-		windowSize = val;
+	// header: bits 112-127
+	void TcpPackage::set_windowSize(uint val){
+		uIntToVector(112,127,header,val);
 	};
-	void TcpPackage::set_checksum(int val){
-		checksum = val;
+	// header: bits 128-143
+	void TcpPackage::set_checksum(uint val){
+		uIntToVector(128,143,header,val);
 	};
-	void TcpPackage::set_options(int* val){
-		options = val;
+	// options: bits 0-15
+	void TcpPackage::set_options(uint* val, uint size){
+		for (int i = 0; i < size; i++){
+			uIntToVector(i, (i + 31), options, val[i]);
+		}
 	};
+	
+	bool* TcpPackage::intToBoolArray(uint val){
+		bool* ret = new bool[32];
+		for(int i = 0; i<32; i++){
+			if (val % 2 == 1)
+				ret[i] = 1;
+			val >> 1;
+		}
+		return ret;
+	};
+	uint TcpPackage::vectorToUInt(int start, int end, std::vector<bool> &set){
+		uint ret = 0;
+		for (int i = start; i <= end; i++){
+			if 	(set.at(i))
+				ret += 1 << (i - start);
+		}
+		return ret;
+	};
+	void TcpPackage::uIntToVector(int start, int end, std::vector<bool> &set, uint val){
+		bool* ins = intToBoolArray(val);
+		for (int i = start; i <= end; i++){
+			set[i] = ins[i];
+		}
+	}; 
 }
