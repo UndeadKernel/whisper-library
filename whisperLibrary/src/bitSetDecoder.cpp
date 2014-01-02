c02e39dc80df1f0d2c6b8a84a66af9d496346ceb
Martin <mac.cat@freenet.de>
2014-01-02 15:53:40 +0100

    implemented feedback for issue 17 reassemble message

--- whisperLibrary/src/bitSetDecoder.cpp
+++ whisperLibrary/src/bitSetDecoder.cpp
@@ -1,28 +1,23 @@
 // Author: Martin Oehler
-
 #include <bitSetDecoder.hpp>
 
 using namespace std;
-
 namespace whisper_library {
 	
-string BitSetDecoder::decodeMessage(vector<bitset<6>> vec) {
-	string bitstream = "";
-
+string BitSetDecoder::decodeMessage(vector<bitset<6>> vector) {
+	string bit_sequence = "";
 	// append bits to string
-	for (unsigned int i = 0; i < vec.size(); ++i) {
-		bitstream += vec[i].to_string();
+	for (unsigned int i = 0; i < vector.size(); ++i) {
+		bit_sequence += vector[i].to_string();
 	}
 
 	// convert binary string to chars
-	string msg = "";
-	for (unsigned int i = 0; i+8 <= bitstream.length(); i += 8) {
-		bitset<8> bs(bitstream.substr(i, 8));
-		char c = static_cast<char>(bs.to_ulong());
-		msg.push_back(c);
+	string message = "";
+	for (unsigned int i = 0; i + 8 <= bit_sequence.length(); i += 8) {
+		bitset<8> bitset(bit_sequence.substr(i, 8));
+		char c = static_cast<char>(bitset.to_ulong());
+		message.push_back(c);
 	}
-
-	return msg;
+	return message;
 }
-
 }
