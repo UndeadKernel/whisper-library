77cd8dbdbb1627cf8c30e5470a3b5d1cce50ca7e
Martin <mac.cat@freenet.de>
2014-01-25 15:43:54 +0100

    removed merge instruction

--- example/main.cpp
+++ example/main.cpp
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 #include "whisperLibrary\channelmanager.hpp"
-=======
-#include <whisperLibrary/socketconnector.hpp>
-#include <whisperLibrary/bitSetEncoder.hpp>
-#include <whisperLibrary/bitSetDecoder.hpp>
->>>>>>> master
-
 #include <iostream>
 
 
@@ -16,4 +9,8 @@ int main(int argc, char* argv[]){
 	whisper_library::ChannelManager channelmanager;
 	vector<string> channels = channelmanager.getChannelNames();
 	vector<string> channel_infos = channelmanager.getChannelInfos();
+
+	for (unsigned int i = 0; i < channels.size(); i++) {
+		cout << "[" << i << "] " << channels[i] << ": " << channel_infos[i] << endl;
+	}
 }
