3754ad69a61d3947cc18f9d22b15bcaa4b745cf4
Jan Simon Bunten <jsbunten@gmail.com>
2014-02-07 08:21:18 +0100

    more doxygen comments inserted

--- whisperLibrary/src/tcppacket.hpp
+++ whisperLibrary/src/tcppacket.hpp
@@ -14,17 +14,27 @@ typedef unsigned long ulong;
 
 namespace whisper_library {
 
-// This class represents a tcp packet. All fields are accessable.
+/** 
+ * \brief This class represents a tcp packet. All fields are accessable.
+ */
 class TcpPacket {
 
 public:
-    // The empty constructor creates an empty tcp packet.
+    /** 
+     * \brief The empty constructor creates an empty tcp packet.
+     */
     TcpPacket();
 	
-    /* 
-		This constructor is used to fill in some of the important fields
-		beforehand.
-    */
+    /**
+     * \brief This constructor is used to fill in some of the important fields beforehand.
+     * \param inSourcePort the source port as integer
+     * \param inDestPort the destination port as integer
+     * \param inSequenceNumber the sequence number of the packet
+     * \param inAckNumber the acknowladgement number of the packet
+     * \param inDataOffset the length of the options field in bytes
+     * \param inWindowSize the size of the packet in bytes
+     * \param inOptions the options as a vector of boolean values
+     */
     TcpPacket(uint inSourcePort, 
 				uint inDestPort, 
 				ulong inSequenceNumber,
@@ -83,44 +93,72 @@ public:
         
     // other public functions
         
-    /* 
-		This function is used to calculate the tcp checksum.
+    /** 
+		\brief This function is used to calculate the tcp checksum.
+		
 		To do this it needs some additional information of the IP header as parameters.
+		\param sourceIp IP adress of the sender as integer
+		\param destIp IP adress of the reciever as integer
+		\param reservedBits the value of the reserved bits of the IP header as integer
+		\param protocol the protocol used to send the data as integer value
     */
     void calculateChecksum(ulong sourceIp, ulong destIp, uint reservedBits, uint protocol);
 
 private: 
-    /* 
-		This function is used to convert a vector into an unsigned integer value.
-		The parameters are start and end bits of the number in the given vector
-		and the vector the number is stored in.
+    /** 
+		\brief This function is used to convert a vector into an unsigned integer value.
+		\param start the starting bit of the value in the given vector
+		\param end the last bit of the value in the given vector
+		\param vec the vector the value is stored in
+		\return the value of the bits as integer
     */
     ulong vectorToULong(int start, int end, const vector<bool> &vec) const;
 
-    /* 
-		This function is used to insert an unsigned integer value into a given boolean vector.
-		The parameters needed are the start and end of where to put the integer
-		into the vector, the vector itself and the value to be inserted.
+    /** 
+		\brief This function is used to insert an unsigned integer value into a given boolean vector.
+		\param start The index where the first bit will be inserted
+		\param end The index where the last bit will be inserted
+		\param vec The vector the value will be inserted in
+		\param val The value to be inserted
+		\tparam T The type of value to be inserted into the vector
     */
     template <class T> void uIntToVector(int start, int end, vector<bool> &vec, T val);
 
-    // This function is used to convert a given integer value into a boolean vector.
+    /** 
+     * \brief This function is used to convert a given integer value into a boolean vector.
+     * \param val The value to be converted
+     * \return A vector of booleans with a binary representation of the value
+     * \tparam T The type of the value to be converted
+     */
     template <class T> vector<bool> intToBoolVector(T val);
 
-    /* 
-		This function uses the one complement addition to add two boolean vectors.
-		The input boolean vectors max size is 16. 
-		The returned sum will have a max size of 16.
+    /** 
+		\brief This function uses the one complement addition to add two 16 bit boolean vectors.
+		\param vec1 The first 16bit boolean vector to be added.
+		\param vec2 The second 16bit boolean vector to be added.
+		\return The 16bit one complement sum of the two given vectors.
     */ 
     vector<bool> oneComplementAdd(vector<bool> vec1, vector<bool> vec2);
 
-    // This function splits a 32 bit boolean vector into two 16 bit vectors
+    /** 
+     * \brief This function splits a 32 bit boolean vector into two 16 bit vectors.
+     * \param vec The vector to be split up.
+     * \return A vector of two 16 bit boolean vectors.
+     */
 	vector<vector<bool> > split32BitVector(vector<bool> vec);
 
-    // This function splits the complete packet into a vector of 16bit boolean vectors.
+    /**
+     * \brief This function splits the whole TCP packet into a vector of 16bit boolean vectors.
+     * \return The TCP packet as a vector of 16bit boolean vectors.
+     */
 	vector<vector<bool> > splitHeaderTo16Bit();
 
-    // This function trims a big endian to a fixed size
+    /** 
+     * \brief This function trims a big endian boolean vector to a fixed size.
+     * \param vec The vector to be trimmed.
+     * \param size The new size of the vector.
+     * \return The vector trimmed to the given size.
+     */
 	vector<bool> trimBigEndianVector(vector<bool> vec, int size);
 
 
