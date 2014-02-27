b431c0ab319db334b265131abe3d27909127c51e
Martin <mac.cat@freenet.de>
2014-02-27 18:03:00 +0100

    tests

--- whisperLibrary/tests/snifferTest.cpp
+++ whisperLibrary/tests/snifferTest.cpp
@@ -15,10 +15,12 @@ BOOST_AUTO_TEST_CASE(sniffer_test) {
 	channelmanager.selectChannel("TCP Header Covert Channel");
 	cout << "current channel: " << channelmanager.currentChannel() << endl;
 	vector<char*> adapters = channelmanager.adapterNames();
+	vector<char*> adapter_descriptions = channelmanager.adapterDescriptions();
 	cout << "Available Adapters:" << endl;
 	for (int i = 0; i < adapters.size(); i++) {
-		cout << adapters[i] << endl;
+		cout << adapters[i] << ": " << adapter_descriptions[i] << endl;
 	}
+	channelmanager.openConnection("127.0.0.81", 8080, adapters[0]);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
