5c78068bf79a3e1f3ff3c1db2ddb515b43e92d2e
Martin <mac.cat@freenet.de>
2014-03-03 13:12:46 +0100

    minor changes

--- whisperLibrary/tests/snifferTest.cpp
+++ whisperLibrary/tests/snifferTest.cpp
@@ -12,26 +12,7 @@ struct SnifferFixture {
 BOOST_FIXTURE_TEST_SUITE(sniffer, SnifferFixture)
 
 BOOST_AUTO_TEST_CASE(sniffer_test) {
-	/*
-	whisper_library::ChannelManager channelmanager;
-	channelmanager.selectChannel("Timing Covert Channel");
-	channelmanager.setErrorStream(&cout);
-	channelmanager.setOutputStream(&cout);
 
-	vector<char*> adapters = channelmanager.adapterNames();
-	vector<char*> adapter_descriptions = channelmanager.adapterDescriptions();
-	cout << "Available Adapters:" << endl;
-	for (int i = 0; i < adapters.size(); i++) {
-		cout << adapters[i] << ": " << adapter_descriptions[i] << endl;
-	}
-
-	channelmanager.openConnection("192.168.2.105", 23, adapters[0]);
-	std::this_thread::sleep_for(chrono::seconds(2));
-	cout << "sending message.." << endl;
-	channelmanager.sendMessage("Hello World.");
-	while (true) {
-
-	}*/
 }
 
 BOOST_AUTO_TEST_SUITE_END()
