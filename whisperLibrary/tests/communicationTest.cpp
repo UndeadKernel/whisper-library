3cdf1be07d7d7bc318bb800f54cbc080d9a7564c
Martin <mac.cat@freenet.de>
2014-03-21 15:46:45 +0100

    added setArgument to system test

--- whisperLibrary/tests/communicationTest.cpp
+++ whisperLibrary/tests/communicationTest.cpp
@@ -78,6 +78,10 @@ struct CommunicationFixture {
 			}
 
 		}
+		if (arguments[0].compare("arguments") == 0) {
+			channelmanager.setChannelArguments(destination_ip, arguments[1]);
+			return 0;
+		}
 		cout << "Command '" << command << "' unknown." << endl;
 		return 0;
 	}
@@ -134,6 +138,7 @@ struct CommunicationFixture {
 		cout << "displayCC: Displays a list of available Covert Channels" << endl;
 		cout << "displayAdapters: Displays a list of available network adapters" << endl;
 		cout << "selectCC [ID]: Select the covert channel with ID [ID]" << endl;
+		cout << "arguments [string]: Gives [string] as an argument to the channel" << endl;
 		cout << "selectAdapter [ID]: Select the network adapter with ID [ID]" << endl;
 		cout << "connect [IP]: Connect to IP using the selected Covert Channel and adapter." << endl;
 		cout << "exit: Exit the chat/program" << endl;
