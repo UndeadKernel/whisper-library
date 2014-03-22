1555fdeb70715851683b0db4a1c9411dfda329df
Martin <mac.cat@freenet.de>
2014-03-22 20:53:42 +0100

    added tests for udppacketgenerator

---
+++ whisperLibrary/tests/udpPacketGeneratorTest.cpp
@@ -0,0 +1,25 @@
+#include <boost/test/unit_test.hpp>
+#include "../src/udppacketgenerator.hpp"
+
+struct UdpPacketGeneratorFixture {
+
+};
+
+BOOST_FIXTURE_TEST_SUITE(udpPacketGenerator, UdpPacketGeneratorFixture);
+
+BOOST_AUTO_TEST_CASE(udp_generate_random_length_test) {
+	whisper_library::UdpPacket packet = whisper_library::UdpPacketGenerator::generateNextPacket(30000);
+	BOOST_CHECK_EQUAL(packet.destinationPort(), 30000);
+	BOOST_CHECK_EQUAL(packet.sourcePort(), 30000);
+	BOOST_CHECK_EQUAL(packet.checksum(), 0);
+	vector<char> data = packet.data();
+	BOOST_CHECK_EQUAL(packet.length(), data.size() + 8);
+	BOOST_CHECK(data.size() >= 50 && data.size() <= 150);
+}
+
+BOOST_AUTO_TEST_CASE(udp_generate_fixed_length_test) {
+	whisper_library::UdpPacket packet = whisper_library::UdpPacketGenerator::generatePacketWithLength(30000, 100);
+	BOOST_CHECK(packet.data().size() == 100-8);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
